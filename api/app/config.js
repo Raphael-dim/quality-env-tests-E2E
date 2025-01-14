52b2020c84d0c1c2d48d702049df62b380ca6c91
Raphael Dimeck <raphael.dimeck@gmail.com>
2025-01-14 14:49:19 +0100

    first commit

---
+++ api/app/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://dbUser:admin@cluster0.fxg17.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/db'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://dbUser:admin@cluster0.fxg17.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/db'
+  }
+}
